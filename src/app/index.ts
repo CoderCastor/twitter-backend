1bb6bd9c5088718408af54e459504f245ecc5c37
codercastor <tejasshinde935@gmail.com>
2024-12-03 21:15:26 +0530

    -- saved at 5:30

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,31 +1,46 @@
 import express from "express";
-import {ApolloServer} from '@apollo/server';
-import {expressMiddleware} from '@apollo/server/express4'
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import bodyParser from "body-parser";
-import {User} from './user'
+import cors from "cors";
+import { User } from "./user";
+import { GraphqlContext } from "./interfaces";
+import JWTService from "../services/jwt";
 
-export async function initServer(){
-    const app = express();
+export async function initServer() {
+  const app = express();
 
-    app.use(bodyParser.json());
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    const graphqlServer = new ApolloServer({
-        typeDefs:`
+  const graphqlServer = new ApolloServer<GraphqlContext>({
+    typeDefs: `
         ${User.types}
         type Query {
             ${User.queries}
         }
         `,
-        resolvers:{
-            Query: {
-                ...User.resolvers.queries,
-            }
-        }
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+      },
+    },
+  });
+
+  await graphqlServer.start();
+
+  app.use(
+    "/graphql",
+    expressMiddleware(graphqlServer, {
+      context: async ({ req, res }) => {
+        return {
+          user: req.headers.authorization
+            ? JWTService.decodeToken(req.headers.authorization)
+            : undefined,
+        };
+      },
     })
+  );
 
-    await graphqlServer.start();
-
-    app.use("/graphql",expressMiddleware(graphqlServer));
-
-    return app;
+  return app;
 }
--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -1,19 +1,24 @@
 import JWT from 'jsonwebtoken'
 import { prismaClient } from '../clients/db'
 import { User } from '@prisma/client';
+import { JWTUser } from '../app/interfaces';
 
 const JWT_SECRET = "$uper@1234."
 
 class JWTService {
     public static generateTokenForUser(user:User){
           
-        const payload = {
+        const payload: JWTUser = {
             id: user?.id,
             email: user?.email
         };
         const token = JWT.sign(payload,JWT_SECRET);
         return token;
     }
+
+    public static decodeToken(token:string){
+        return JWT.verify(token,JWT_SECRET) as JWTUser;
+    }
 }
 
-export default JWTService;
+export default JWTService; 
